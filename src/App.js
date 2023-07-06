40c2a3c791dd86f321366ee11776089e8be99d16
Felipe <felipesullivan50@gmail.com>
2023-07-06 08:34:53 -0300

    reaf: volta para App.js como estava antes, remove pasta Main de components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { Container } from "./components/Containers/Container";
+import { Section } from "./components/Containers/Section";
+import { Card } from "./components/Layout/Card";
+import { Footer } from "./components/Layout/Footer";
+import { Header } from "./components/Layout/Header.js";
+import { Profile } from "./components/Layout/Profile";
 import { APIProvider } from "./data/contexts/APIContext";
-import { Main } from "./components/Layout/Main";
 
-function App() {
+function Main() {
   return (
     <APIProvider>
-      <Main />
+      <Container>
+        <Header />
+        <Section className="profile">
+          <Profile />
+        </Section>
+        <Section className="cards">
+          <Card />
+        </Section>
+        <Footer />
+      </Container>
     </APIProvider>
   );
 }
 
-export default App;
+export default Main;
